c6dff5676a8b243eb17c4c88d54248f460b2421e
Travis Waelbroeck <twaelbroeck@gmail.com>
2018-11-05 13:40:31 -0600

    [UPD] Check each line for Git conflicts, Check for `=======`
    Rather than just checking the entire file as a string, this will now check each individual line.
    
    There are two general checks being run:
    
    * Does the line contain `<<<<<<<` or `>>>>>>>`?
    * Is the line an exact match for `=======` or `HEAD`?
    
    If either of those two criteria are found, pre-commit will fail the commit attempt.
    
    Example output of an error:
    
    ```
    [forbid-git-conflicts] Check for leftover Git conflicts.......................Failed
    hookid: forbid-git-conflicts
    
    Git Conflicts Detected in file(s):
     - bhs/models/sale/order.py:17 | <<<<<<< HEAD
     - bhs/models/sale/order.py:19 | =======
     - bhs/models/sale/order.py:21 | >>>>>>> master
    ```

--- README.md
+++ README.md
@@ -4,3 +4,37 @@
 > find. This repository is shadowed from several repositories, but namely
 > [Lucas-C's pre-commit-hooks repository](https://github.com/Lucas-C/pre-commit-hooks)
 > and some of the others mentioned on the [pre-commit hooks page](https://pre-commit.com/hooks.html).
+
+## Supported Hooks
+
+### forbid-git-conflicts
+
+Simple hook which iterates over all staged files to check for any lines that
+have left over Git conflict markers.
+
+* Does the line contain `<<<<<<<` or `>>>>>>>`?
+* Is the line an exact match for `=======` or `HEAD`?
+
+If either of those two criteria are found, pre-commit will fail the commit attempt.
+
+#### .pre-commit-config.yaml Example
+
+```
+repos:
+-   repo: https://github.com/traviswaelbro/pre-commit-hooks
+    rev: master
+    hooks:
+    -   id: forbid-git-conflicts
+```
+
+#### Failure Output Example
+
+```
+[forbid-git-conflicts] Check for leftover Git conflicts.......................Failed
+hookid: forbid-git-conflicts
+
+Git Conflicts Detected in file(s):
+ - helpers/example.py:17 | <<<<<<< HEAD
+ - helpers/example.py:19 | =======
+ - helpers/example.py:21 | >>>>>>> master
+```
--- hooks/forbid_git_conflicts.py
+++ hooks/forbid_git_conflicts.py
@@ -3,11 +3,13 @@ import argparse, sys
 from helpers import is_textfile
 
 
-CONFLICT_STRINGS = ['<<<<<<<',
-                    '>>>>>>>', ]
+CONTAINS_CONFLICT_STRINGS = ['<<<<<<<',
+                             '>>>>>>>', ]
+EXACT_CONFLICT_STRINGS = ['=======',
+                          'HEAD', ]
 
 
-def contains_git_conflicts(filename):
+def find_git_conflicts(filename):
     """
     Check the given file for any of the CONFLICT_STRINGS.
 
@@ -16,35 +18,79 @@ def contains_git_conflicts(filename):
       - If any of the git conflicts are in a comment, this may report them as a
         failure even though it may not actually cause any issue.
 
+    :param filename {str}: file name with relative path
     :return {bool}: True if conflicts are found else False
     """
-    with open(filename, mode='rb') as file_checked:
-        file_contents = file_checked.read()
-        return any(conflict_string in file_contents for conflict_string in CONFLICT_STRINGS)
+    with open(filename, mode='r') as file_checked:
+        git_conflicts = []
+        for line_number, line in enumerate(file_checked, start=1):
+            if has_contained_conflict(line) or has_exact_conflict(line):
+                git_conflicts.append("{}:{} | {}".format(filename, line_number, line))
+
+        return git_conflicts
+
+
+def has_contained_conflict(content):
+    """
+    Check if the provided content contains a match for any of the
+    CONTAINS_CONFLICT_STRINGS values.
+
+    Lines may have extra content and still be considered a conflict.
+
+    :param content {str}: content to be checked for conflicts
+    :return {bool}:
+        True if the provided content contains a match for any of the
+        CONFLICT_STRINGS values.
+    """
+    return any(conflict_string in content.rstrip('\n') for conflict_string in CONTAINS_CONFLICT_STRINGS)
+
+
+def has_exact_conflict(content):
+    """
+    Check if the provided content contains a match for any of the
+    CONTAINS_CONFLICT_STRINGS values.
+
+    Lines must be an exact match (no extra content) to be considered a conflict.
+
+    :param content {str}: content to be checked for conflicts
+    :return {bool}:
+        True if the provided content is an exact match for any of the
+        EXACT_CONFLICT_STRINGS values.
+    """
+    return any(conflict_string == content.rstrip('\n') for conflict_string in EXACT_CONFLICT_STRINGS)
 
 
 def main(argv=None):
     """
-    Check files for any left over Git conflicts.
+    Parse each line of the staged files to check for left over Git conflicts.
+
+    Errors will be thrown for any lines that:
+      - Contain:
+          - '<<<<<<<'
+          - '>>>>>>>'
+      - Are equal to (excluding line endings):
+          - '======='
+          - 'HEAD'
+
+    :return {int}: 1 if failure else 0
     """
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='File names to check')
     args = parser.parse_args(argv)
     filenames = args.filenames
-    #  parser.add_argument('--files', '-f', help='File names to check')
-    #  args = parser.parse_args(argv)
-    #  filenames = args.files.split(',')
 
     # Find files with conflicts
     text_files = [filename for filename in filenames if is_textfile(filename)]
-    files_with_conflicts = [text_file for text_file in text_files if contains_git_conflicts(text_file)]
+    files_with_conflicts = []
+    for text_file in text_files:
+        files_with_conflicts += find_git_conflicts(text_file)
 
     # Return response
     exit_code = 0
     if files_with_conflicts:
         exit_code = 1
-        print('Git Conflicts Detected in file(s): \n - {}'.format('\n - '.join(files_with_conflicts)))
+        print('Git Conflicts Detected in file(s): \n - {}'.format(' - '.join(files_with_conflicts)))
 
     return exit_code
 
